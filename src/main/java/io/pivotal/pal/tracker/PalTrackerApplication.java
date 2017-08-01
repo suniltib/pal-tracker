d085013b575a46cc64a603f261221bb0eb631984
accenturelabs <accenturelabs@accenturelabss-iMac.local>
2017-08-01 16:41:27 -0400

    JDBC

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,17 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 
+import javax.sql.DataSource;
+
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository() throws Exception{
+        DataSource dataSource = new MariaDbDataSource(System.getenv("SPRING_DATASOURCE_URL"));
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
