0a8593e118514830b3eef36350fa119673871d5b
accenturelabs <accenturelabs@accenturelabss-iMac.local>
2017-08-01 09:05:32 -0400

    push

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String hello;
+
+    public WelcomeController(@Value("${welcome_message}") String message) {
+        hello = message;
+    }
+
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return hello;
     }
+
 
 }
